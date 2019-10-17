25b2938fe9bd41ff2088d70b562c6acce935a56f
Cory Lynch <cjl2625@gmail.com>
2019-10-17 02:12:58 -0400

    Added teleportation to other scenes

---
+++ Assets/Scripts/SceneTeleporter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SceneTeleporter : MonoBehaviour {
+    public string targetSceneName;
+    public TextMeshPro textComponent;
+
+    private bool loading = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !loading)
+        {
+            print("loading scene " + targetSceneName);
+            loading = true;
+            textComponent.text = "Loading new scene...";
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(targetSceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
