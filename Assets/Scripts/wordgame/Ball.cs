2c0f61fcf8dc1fc9ed3d62391106b364a60155d6
Cory Lynch <cjl2625@gmail.com>
2020-02-22 04:52:25 -0500

    Add some game testing for wordgame

---
+++ Assets/Scripts/wordgame/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Renderer renderer;
+    private bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead)
+        {
+            Color oldCol = renderer.material.color;
+            if (oldCol.a <= 0)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Color newCol = new Color(oldCol.r, oldCol.g, oldCol.b, oldCol.a - 1.5f*Time.deltaTime);
+            renderer.material.color = newCol;
+            Debug.Log(newCol);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Weapon"))
+        {
+            dead = true;
+            //Instantiate(explode, location.position, transform.rotation);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/wordgame/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBall", 2.0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnBall()
+    {
+        GameObject instance = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        Rigidbody rb = instance.GetComponent<Rigidbody>();
+
+        rb.velocity = new Vector3(0, Random.Range(0, 15), Random.Range(25,40));
+
+    }
+}
