c2e545d2e0040ed6ec508ee5d6c0d1c40f67e15b
Daniel Dangond <dadangond@gmail.com>
2020-03-02 23:57:11 -0500

    Added some drinks to the supermarket and labelled the signs

---
+++ Assets/Scripts/Supermarket/Sign.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Sign : MonoBehaviour
+{
+    public string text = "Text";
+
+    private List<TMPro.TextMeshPro> tms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tms = new List<TMPro.TextMeshPro>(GetComponentsInChildren<TMPro.TextMeshPro>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (TMPro.TextMeshPro tm in tms)
+        {
+            tm.text = text;
+        }
+    }
+}
