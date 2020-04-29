d300916fe5ff83373b17f58c5ce4f6661fb3fb1a
Cory Lynch <cjl2625@gmail.com>
2020-04-29 02:47:40 -0400

    Finish question editor

--- game-editor/public/index.js
+++ game-editor/public/index.js
@@ -1,63 +1,87 @@
 const state = {
-    output: {
-        sentences: [],
-        kanjiList: "",
-    }
+  output: {
+    sentences: [],
+    kanjiList: "",
+  },
 };
 
 function clear() {
-    document.querySelector("#sentence-input").value = "";
-    document.querySelector("#display").innerHTML = "";
-    document.querySelector("#current").innerHTML = "";
+  document.querySelector("#sentence-input").value = "";
+  document.querySelector("#display").innerHTML = "";
+  document.querySelector("#current").innerHTML = "";
+  document.querySelector("#current-box").style.display = "none";
 }
 
 function submit() {
-    const text = document.querySelector("#sentence-input").value;
-    const display = document.querySelector("#display");
-    clear();
+  const text = document.querySelector("#sentence-input").value;
+  const display = document.querySelector("#display");
+  clear();
 
-    state.text = text;
-    state.questions = [];
-    [...text].forEach((char, i) => {
-        const charSpan = document.createElement('span');
-        charSpan.setAttribute('onclick', `create('${char}', ${i})`);
-        charSpan.innerText = char;
-        display.appendChild(charSpan)
-    });
+  state.text = text;
+  state.questions = [];
+  [...text].forEach((char, i) => {
+    const charSpan = document.createElement("span");
+    charSpan.setAttribute("onclick", `create('${char}', ${i})`);
+    charSpan.innerText = char;
+    display.appendChild(charSpan);
+  });
 }
 
 function create(char, i) {
-    const answer = prompt(`Enter the reading for ${char}`);
-    const current = document.querySelector("#current");
-    state.questions.push({
-        i,
-        content: char,
-        answer
-    });
-    current.innerHTML += `<div>${i}: ${char} -> ${answer}</div>`
+  const reading = prompt(`Enter the reading for ${char}`);
+  const current = document.querySelector("#current");
+  state.questions.push({
+    i,
+    content: reading,
+    answer: char,
+  });
+  current.innerHTML += `<li><span class="u-bold">Index ${i}:</span> ${char} → ${reading}</li>`;
+  document.querySelector("#current-box").style.display = "flex";
 }
 
 function finishSentence() {
-    if (state.questions.length == 0) return alert("Add some readings first");
+  if (state.questions.length == 0) return alert("Add some readings first");
 
-    const parts = [];
-    state.questions.sort((a, b) => a.i - b.i);
-    state.questions.forEach((q, i) => {
-        const prevIndex = i && state.questions[i-1].i + 1;
-        const prev = state.text.substring(prevIndex, q.i);
-        if (prev) {
-            parts.push({isQuestion: false, content: prev});
-        }
-        parts.push({isQuestion: true, content: q.content, answer: q.answer});
-    });
-
-    const finalQuestion = state.questions[state.questions.length-1];
-    if (finalQuestion.i < state.text.length - 1) {
-        parts.push({isQuestion: false, content: state.text.substring(finalQuestion.i + 1, state.text.length)});
+  const parts = [];
+  state.questions.sort((a, b) => a.i - b.i);
+  state.questions.forEach((q, i) => {
+    const prevIndex = i && state.questions[i - 1].i + 1;
+    const prev = state.text.substring(prevIndex, q.i);
+    if (prev) {
+      parts.push({ isQuestion: false, content: prev });
     }
+    parts.push({ isQuestion: true, content: q.content, answer: q.answer });
+  });
 
-    state.output.sentences.push({ parts });
-    state.output.kanjiList = document.querySelector("#kanji-list").value;
-    document.querySelector("#output").innerHTML = JSON.stringify(state.output, null, 2);
-    clear();
+  const finalQuestion = state.questions[state.questions.length - 1];
+  if (finalQuestion.i < state.text.length - 1) {
+    parts.push({
+      isQuestion: false,
+      content: state.text.substring(finalQuestion.i + 1, state.text.length),
+    });
+  }
+
+  state.output.sentences.push({ parts });
+  state.output.kanjiList = document.querySelector("#kanji-list").value;
+
+  const text = [...state.text]
+    .map((char, i) =>
+      state.questions.some((q) => q.i === i)
+        ? `<span class="emph">${char}</span>`
+        : char
+    )
+    .join("");
+  document.querySelector("#output").innerHTML += `<li>${text}</li>`;
+  document.querySelector("#output-box").style.display = "flex";
+  clear();
+}
+
+function saveFile() {
+  fetch("/api/submit", {
+    method: "post",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(state.output),
+  })
+    .then((res) => res.json())
+    .then((res) => alert("Saved!"));
 }
--- game-editor/server.js
+++ game-editor/server.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
 
-app.get('/test', (req, res) => res.send('Hello World!'))
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
-app.use(express.static('public'))
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
+app.post("/api/submit", (req, res) => {
+  fs.writeFileSync(
+    "../Assets/Scripts/wordgame/gameconfig.json",
+    JSON.stringify(req.body)
+  );
+  res.send({});
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use(express.static("public"));
+
+const port = 3000;
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
